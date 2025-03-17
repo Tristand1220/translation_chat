7aa74794febb92933f2e5dd1d89a82dec8964afa
[tdaniel60] <tdaniel60@gatech.edu>
2025-03-17 12:58:23 +0800

    Chatroom functionalities part1
    Half-way through server improvements, UI design started

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const PORT = process.env.PORT || 3500
+const ADMIN = "Admin"
 
 const app = express()
 
@@ -15,6 +16,14 @@ app.use(express.static(path.join(__dirname, "public")))
 const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
+
+// state
+const UserState = {
+    users: [],
+    setUsers: function (newUsersArray){
+        this.users = newUsersArray
+    }
+}
 
 const io = new Server(expressServer, {
     cors: {
@@ -26,27 +35,120 @@ io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
     //Upon connection, sends only to user
-    socket.emit('message', "Welcome to practiLang !")
+    socket.emit('message', buildMsg(ADMIN, "Welcome to practiLang !"))
 
+    socket.on('enterRoom', ({ name, room }) => {
+        //leave previous chat room
+        const prevRoom = getUser(socket.id)?.room
+        if (prevRoom){
+            socket.leave(prevRoom)
+            io.to(prevRoom).emit('message', buildMsg(ADMIN, `${name} has left the room`))
+        }
+
+        const user = activateUser(socket.id, name, room)
+
+        if (prevRoom) {
+            io.to(prevRoom).emit('AIsuggest', {
+                users: getUsersInRoom(prevRoom)
+            })
+        }
+
+        // join room
+        socket.join(user.room)
+
+        //To user who joins room
+        socket.emit('message', buildMsg(ADMIN, `You have joined the ${user.room} chat room`))
+
+        //To everyone else (new user joining)
+        socket.broadcast.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has joined the room`))
+
+        //Update user list for room (where the suggestion will go.....)
+        io.to(user.room).emit('AIsuggest', {
+            users: getUserinRoom(user.room)
+        })
+
+        // Update similarity
+        io.emit('similar', {
+            rooms: getAllActiveRooms()
+        })
+    })
+    
     //Upon connection, sends to all other
     socket.broadcast.emit('message', `User ${socket.id.substring(0,5)}} connected`)
 
-    // Listening for message event
-    socket.on('message', data => {
-        console.log(data)
-        // Sending message and who it came from  to all connected to server
-        io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
+     // When user disconnects, sends to all others
+     socket.on('disconnect', () => {
+        const user = getUser(socket.id)
+        leaveApp(socket.id)
+        
+        if(user) {
+            io.to(user.room).emit('message', buildMsg(ADMIN, `${user.name} has left the room`))
+        }
+        console.log(`User ${socket.id} disconnected`)
     })
 
-    // When user disconnects, sends to all others
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)}} disconnected`)
+    // Listening for message event
+    socket.on('message', ({name,text}) => {
+        const room =getUser(socket.id)?.room
+
+        // Sending message and who it came from to all connected to server
+        if (room) {
+            io.to(room).emit('message', buildMsg(name, text))
+        }
     })
 
     // Listen for activity (keypress)
     socket.on('activity', (name) => {
+        const room =getUser(socket.id)?.room
         // Sends "{user} is typing..." to everyone else
-        socket.broadcast.emit('activity', name)
+        if (room) {
+            socket.broadcast.to(room).emit('activity', name)
+        }
     })
 })
 
+// Creates our text; with name, message, and time sent
+function buildMsg(name,text){
+    return{
+        name,
+        text,
+        time: new Intl.DateTimeFormat('default', {
+            hour: 'numeric',
+            minute: 'numeric'
+        }).format(new Date())
+    }
+}
+
+// User functions
+
+// Adds user to a room and makes sure they're not a duplicate
+function activateUser(id,name,room){
+    const user = { id, name, room} // User is their id,name,and room
+    UsersState.setUsers([
+        ...UserState.users.filter(user => user.id !== id),
+        user
+    ])
+    return user
+}
+
+//User leaving application
+function leaveApp(id){
+    UserState.setUsers(
+        UserState.users.filter(user => user.id !== id)
+    )
+}
+
+// Get user
+function getUser(id){
+    return UserState.users.find(user => user.id === id)
+}
+
+// Get user room
+function getUserinRoom(room){
+    return UsersState.users.filter(user => user.room == room)
+}
+
+//Get all active chatrooms (set to avoid duplicates)
+function getAllActiveRooms(){
+    return Array.from(new Set(UsersState.users.map(user => user.room)))
+}
--- server/public/app.js
+++ server/public/app.js
@@ -1,21 +1,49 @@
 const socket = io('ws://localhost:3500')
 
 //Watching typing activity
+const msgInput = document.querySelector('#message')
 
+//Watching chatname and target language
+const nameInput = document.querySelector('#name')
+const langRoom = document.querySelector('#langauge')
+
+// AI suggestions and similarity
+const aiSuggestion = document.querySelector('.AIsuggest')
+const similarity = document.querySelector('.similar')
 const activity = document.querySelector('.activity')
-const msgInput = document.querySelector('input')
+
+//Chat Display
+const chatDisplay = document.querySelector('.chat-display')
 
 function sendMessage(e){
     e.preventDefault()
-    if (msgInput.value) {
-        socket.emit('message', msgInput.value)
+    if (nameInput.value && msgInput.value && langRoom.value) {
+        socket.emit('message', {name: nameInput.value,
+            text:msgInput.value})
         msgInput.value = ""
     }
     msgInput.focus()
 }
 
+//Entering correct chatroom
+function enterRoom(e){
+    e.preventDefault()
+    if (nameInput.value && langRoom.value){
+        socket.emit('enterRoom', {
+            name: nameInput.value,
+            room: langRoom.value
+        })
+    }
+}
+
 // Submits form by applying the message function when submit happens
-document.querySelector('form').addEventListener('submit', sendMessage)
+document.querySelector('form-msg').addEventListener('submit', sendMessage)
+document.querySelector('form-join').addEventListener('submit', enterRoom)
+
+// Sending user name who's typing (activity)
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', nameInput.value)
+})
 
 // Listen for messages
 socket.on("message", (data) => {
@@ -25,10 +53,7 @@ socket.on("message", (data) => {
     document.querySelector('ul').appendChild(li)
 })
 
-// Sending activity event to server along with id
-msgInput.addEventListener('keypress', () => {
-    socket.emit('activity', socket.id.substring(0, 5))
-})
+
 
 // Timer for typing message
 let activityTimer
